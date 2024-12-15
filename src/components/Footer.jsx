b92b9eb1b033398c193bcfba0da4afdaf280a48e
shahidhustles <shahidpatel.1666@gmail.com>
2024-12-15 14:08:24 +0530

    enhance Footer component:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { logo } from "../assets/images";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section
+      id="footer"
+      className="min-h-[100px] flex flex-row items-center justify-evenly bg-grey mt-8"
+    >
+      <div>
+        <img src={logo} alt="logo" height={75} width={150} className="rounded-2xl" />
+      </div>
+      <span className="text-white font-montserrat font-semibold">
+        <p>Made with ❤️ by CS-I G4 </p>
+        <p>&copy; CE Department, CS-I Group: 4</p>
+      </span>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
