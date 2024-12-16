5806d42c19d3a89a31c5ba581233befd0b6c9783
shahidhustles <shahidpatel.1666@gmail.com>
2024-12-16 14:02:58 +0530

    use environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9u8_wdr1v1MT_lEI7SsHrkUQg2NLsjAc",
-  authDomain: "greentex-f8095.firebaseapp.com",
-  projectId: "greentex-f8095",
-  storageBucket: "greentex-f8095.firebasestorage.app",
-  messagingSenderId: "582104727416",
-  appId: "1:582104727416:web:9d46fa50184b670818528d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
